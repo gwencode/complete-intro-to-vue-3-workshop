6f975c3c06fb40515faed82a5bcb593c44681574
gwendal le bris <gwen.coding@gmail.com>
2023-09-21 14:06:29 +0200

    First Component

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,60 +1,25 @@
 <script>
-  export default {
-    data() {
-      return {
-        message: "Hello it works!",
-        counterTitle: "Counter Standard",
-        items: [
-            { name: "Item 1", list:  [1, 2, 3] },
-            { name: "Item 2", list:  [4, 5, 6] },
-            { name: "Item 3", list:  [7, 8, 9] }
-          ],
-        count: 10,
-        incrementAmount: 8
-        }
-      },
-      computed: {
-        displayTitle() {
-          if (this.count > 20 ) {
-            return 'Counter Standard - Very Long'
-          } else {
-            return 'Counter Standard'
-          }
-        },
-        optimizedIncrementAmount() {
-          return this.displayTitle.length * this.incrementAmount
-        }
-      },
-      methods: {
-        incrementCount(newAmount, event) {
-          // console.log(newAmount);
-          // console.log(event);
-          this.count += this.optimizedIncrementAmount
-      },
-      // watch: {
-      //   count(newValue) {
-      //     if (newValue > 20) {
-      //       this.counterTitle += ' Very Long'
-      //     }
-      //   }
-      // }
+import BaseCounter from './components/BaseCounter.vue'
+
+export default {
+  components: {
+    BaseCounter
+  },
+  data() {
+    return {
+      message: "Hello it works!",
+      items: [
+          { name: "Item 1", list:  [1, 2, 3] },
+          { name: "Item 2", list:  [4, 5, 6] },
+          { name: "Item 3", list:  [7, 8, 9] }
+        ],
+      }
     }
   }
 </script>
 
 <template>
-  <h1> {{ displayTitle }} </h1>
-  <p> {{ count }} </p>
-  <button @click="incrementCount(incrementAmount, $event)">Increment count</button>
-
-  <div style="margin-top: 16px">
-    <label for="incrementAmount">Increment by:</label>
-    <input type="number" v-model="incrementAmount">
-  </div>
-
-  <h3>Increment Amount: {{ incrementAmount }}</h3>
-  <h3>Optimized Increment Amount: {{ optimizedIncrementAmount }}</h3>
-
+  <BaseCounter />
   <hr />
 
   <h1>Hello!</h1>
---
+++ sandbox/src/components/BaseCounter.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data: () => ({
+    count: 10,
+    counterTitle: "Counter Standard",
+    incrementAmount: 8
+  }),
+  computed: {
+      displayTitle() {
+        if (this.count > 20 ) {
+          return 'Counter Standard - Very Long'
+        } else {
+          return 'Counter Standard'
+        }
+      },
+      optimizedIncrementAmount() {
+        return this.displayTitle.length * this.incrementAmount
+      }
+    },
+    methods: {
+      incrementCount(newAmount, event) {
+        console.log(newAmount);
+        console.log(event);
+        this.count += this.optimizedIncrementAmount
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> {{ displayTitle }} </h1>
+  <p> {{ count }} </p>
+  <button @click="incrementCount(incrementAmount, $event)">Increment count</button>
+
+  <div style="margin-top: 16px">
+    <label for="incrementAmount">Increment by:</label>
+    <input type="number" v-model="incrementAmount">
+  </div>
+
+  <h3>Increment Amount: {{ incrementAmount }}</h3>
+  <h3>Optimized Increment Amount: {{ optimizedIncrementAmount }}</h3>
+
+</template>
