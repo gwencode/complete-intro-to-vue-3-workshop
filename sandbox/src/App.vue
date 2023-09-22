82d66fcffb2e7262d645b0a5befa15a1f5c8a982
gwendal le bris <gwen.coding@gmail.com>
2023-09-22 16:52:40 +0200

    Composition API : change fetching pokedex -> from created to async setup of Pokedex component. Will not work without Suspense component

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,29 +1,15 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon?limit=151
+import FullPokedex from "./components/FullPokedex.vue"
 export default {
-  data: () => ({
-      pokedex: [1, 2, 3],
-    }),
-  methods: {
-    async fetchPokemon() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-      const data = await response.json();
-      this.pokedex = data.results;
-    }
-  },
-  created() {
-    console.log('created');
-    this.fetchPokemon();
-    console.log(this.pokedex);
+  components: {
+    FullPokedex,
   }
 }
-
 </script>
 
 <template>
   <h1>New App</h1>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
-  <pre> {{ pokedex }}</pre>
+  <FullPokedex />
 </template>
 
 <style>
---
+++ sandbox/src/components/FullPokedex.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+  async setup() {
+    // Pure VanillaJS inside setup. Be very explicit and return
+    const regionName = 'Kanto';
+
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const data = await response.json();
+    const pokedex = data.results;
+
+    return {
+      pokedex,
+      regionName,
+    };
+  },
+  created() {
+    console.log(this.regionName);
+    console.log(this.pokedex);
+  }
+}
+</script>
+
+<template>
+  <h3>Pokedex of {{ this.regionName }}</h3>
+  <pre> {{ this.pokedex }}</pre>
+</template>
