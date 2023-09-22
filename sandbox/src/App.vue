7f6b5e8db52fbc015d6fb7907d4ad0eaf2174481
gwendal le bris <gwen.coding@gmail.com>
2023-09-22 14:53:59 +0200

    feature: add pokedex fetch api call

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,9 +1,29 @@
 <script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+export default {
+  data: () => ({
+      pokedex: [],
+    }),
+  methods: {
+    async fetchPokemon() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const data = await response.json();
+      this.pokedex = data.results;
+    }
+  },
+  created() {
+    console.log('created');
+    this.fetchPokemon();
+    console.log(this.pokedex);
+  }
+}
 
 </script>
 
 <template>
   <h1>New App</h1>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <pre> {{ pokedex }}</pre>
 </template>
 
 <style>
