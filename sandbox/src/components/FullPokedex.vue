18a345df2cbb3f6a6d6e130f58c4acb2c1f48456
gwendal le bris <gwen.coding@gmail.com>
2023-09-25 17:56:26 +0200

    feature: refactor using script setup

--- sandbox/src/components/FullPokedex.vue
+++ sandbox/src/components/FullPokedex.vue
@@ -1,53 +1,44 @@
-<script>
+<script setup>
 
+import BaseButton from "./BaseButton.vue";
 import { computed, ref, reactive } from "vue";
 
-export default {
-  async setup() {
-    // Pure VanillaJS inside setup. Be very explicit and return
-    const regionName = ref('Kanto');
+// Pure VanillaJS inside setup. Be very explicit and return
+const regionName = ref('Kanto');
 
-    const regionNameUpperCase = computed(() => {
-      console.log(regionName);
-      return regionName.value.toUpperCase();
-      // With ref, regionName is not a string but a RefImpl
-    });
+const regionNameUpperCase = computed(() => {
+  console.log(regionName);
+  return regionName.value.toUpperCase();
+  // With ref, regionName is not a string but a RefImpl
+});
 
-    const state = reactive({
-      elementType: 'lightning'
-    });
+const state = reactive({
+  elementType: 'lightning'
+});
 
-    const elementTypeUpperCase = computed(() => {
-      return state.elementType.toUpperCase();
-    })
+const elementTypeUpperCase = computed(() => {
+  return state.elementType.toUpperCase();
+})
 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-    const data = await response.json();
-    const pokedex = data.results;
+const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+const data = await response.json();
+const pokedex = data.results;
 
-    const changeRegionName = () => {
-      regionName.value = "Hoenn";
-    }
-
-    return {
-      elementTypeUpperCase,
-      changeRegionName,
-      pokedex,
-      regionName,
-      regionNameUpperCase,
-    };
-  }
-  // computed: {
-  //   regionNameLowerCase() {
-  //     return this.regionName.toLowerCase()
-  //   }
-  // },
-  // methods: {
-  //   changeRegionName() {
-  //     this.regionName = "Hoenn";
-  //   }
-  // }
+const changeRegionName = () => {
+  regionName.value = "Hoenn";
 }
+
+// computed: {
+//   regionNameLowerCase() {
+//     return this.regionName.toLowerCase()
+//   }
+// },
+// methods: {
+//   changeRegionName() {
+//     this.regionName = "Hoenn";
+//   }
+// }
+
 </script>
 
 <template>
@@ -56,5 +47,6 @@ export default {
   <!-- <h3>Region Name Lowercase : {{ regionNameLowerCase }}</h3> -->
   <button @click="changeRegionName">Change Region Name</button>
   <h3>Element Type Uppercase (with reactive => Proxy) : {{ elementTypeUpperCase }}</h3>
+  <BaseButton left="true" />
   <pre> {{ pokedex }}</pre>
 </template>
