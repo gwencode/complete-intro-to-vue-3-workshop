ae1a32fd7067e870bbcb05e5caf4b273f01724e4
gwendal le bris <gwen.coding@gmail.com>
2023-09-25 16:27:00 +0200

    Make users reactive using ref

--- cest-la-vue/src/components/UsersPage.vue
+++ cest-la-vue/src/components/UsersPage.vue
@@ -1,17 +1,32 @@
 <script>
 import UserCard from './UserCard.vue';
+import { computed, ref, reactive } from "vue";
 
 export default {
   components: {
     UserCard
   },
+
   async setup() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const users = await response.json();
+    const data = await response.json();
+    const users = ref(data);
     console.log(users);
 
     return {
       users
+    }
+  },
+  methods: {
+    changeFirstUser() {
+      this.users[0] = {
+        id: 11,
+        name: "Gwendal LE BRIS",
+        username: "gwencode",
+        email: "gwen@me.com",
+        phone: "06 35 22 29 54"
+      };
+      console.log(this.users[0]);
     }
   }
 }
@@ -20,6 +35,7 @@ export default {
 
 <template>
   <h1>Users</h1>
+  <button @click="changeFirstUser">Change First User</button>
   <UserCard
     v-for="user in users"
     :key="`user-${user.id}`"
