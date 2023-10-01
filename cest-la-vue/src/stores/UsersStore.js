8e63be5051cc3dd270b47a294cc3139c11a9bf1c
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 16:03:47 +0200

    State management Exercice with Pinia: Create Store

---
+++ cest-la-vue/src/stores/UsersStore.js
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+
+export const UsersStore = defineStore('UsersStore', {
+  // Data
+  state: () => ({
+    newUsers: []
+  }),
+
+  // Computed
+  getters: {
+
+  },
+
+  // Methods
+  actions: {
+    async fetchUsers() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await response.json();
+      this.newUsers = data;
+    },
+    newChangeFirstUser() {
+      this.newUsers[0] = {
+        id: 1,
+        name: "Gwendal LE BRIS",
+        username: "gwencode",
+        email: "gwen@me.com",
+        phone: "06 35 22 29 54"
+      };
+    },
+    newChangeSecondUser() {
+      this.newUsers[1] = {
+        id: 2,
+        name: "Corentin LE BRIS",
+        username: "coco22",
+        email: "coco@me.com",
+        phone: "06 00 00 00 00"
+      };
+    },
+  },
+})
