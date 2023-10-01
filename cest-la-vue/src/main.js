cdc28e4480061398125274df379ca97b71f99eba
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 15:46:33 +0200

    State management Exercice with Pinia: Install and Configure

--- cest-la-vue/src/main.js
+++ cest-la-vue/src/main.js
@@ -2,6 +2,7 @@
 
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { createPinia } from 'pinia'
 import { routes } from './router.js'
 import App from './App.vue'
 
@@ -10,6 +11,8 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 app.mount('#app')
