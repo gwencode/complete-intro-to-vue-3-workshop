8c86794b872ca0d285164eff04e5cca3c12f2b09
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 11:19:22 +0200

    Programmatic Navigation

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-import BaseCounter from "./components/BaseCounter.vue"
-import UserCard from "./components/UserCard.vue"
-import { ref } from 'vue';
+import BaseCounter from "@/components/BaseCounter.vue";
+import UserCard from "@/components/UserCard.vue";
+import { newCount } from '@/composables/useCountStore';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
-const colorPreference = ref('#ffffff')
+const colorPreference = ref('#ffffff');
+const countStore = newCount;
+const router = useRouter();
+
+watch(countStore, (val) => {
+  if (val > 140) {
+    router.push("/pokedex");
+  }
+});
 
 </script>
 
@@ -13,11 +23,14 @@ const colorPreference = ref('#ffffff')
       <router-link to="/">Home</router-link>
       <router-link to="/pokedex">Pokedex</router-link>
     </nav>
-    <router-view />
-    <h2>{{ colorPreference }} </h2>
-    <input type="color" v-model="colorPreference">
-    <BaseCounter />
-    <UserCard :user="{ name: 'Ben', age: 28 }"  />
+    <h2> {{ countStore }} </h2>
+    <main>
+      <router-view />
+      <h2>{{ colorPreference }} </h2>
+      <input type="color" v-model="colorPreference">
+      <BaseCounter />
+      <UserCard :user="{ name: 'Ben', age: 28 }"  />
+   </main>
   </div>
 
 </template>
@@ -32,5 +45,27 @@ html {
 }
 .button {
   border: 10px solid red !important
+}
+
+nav {
+  display: flex;
+  justify-content: flex-start;
+  padding: 1rem;
+  background-color: #d1d1d1;
+  color: #fff;
+}
+
+nav a {
+  color: black;
+  text-decoration: none;
+  margin-right: 8px;
+}
+
+main {
+  padding: 1rem;
+}
+
+h1 {
+  margin-top: 0;
 }
 </style>
