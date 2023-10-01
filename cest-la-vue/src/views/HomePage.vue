1f3b96089629745c639e9dd4cfe00b5c19d7c461
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 16:04:14 +0200

    State management Exercice with Pinia: Add functionalities to HomePage view using store

--- cest-la-vue/src/views/HomePage.vue
+++ cest-la-vue/src/views/HomePage.vue
@@ -1,11 +1,21 @@
 <script setup>
 import { computed } from "vue";
 import { users } from '../composables/useUserStore';
+import { UsersStore } from "@/stores/UsersStore";
+
 import { changeFirstUser, changeSecondUser } from '../composables/useUserFonction';
 import UserCard from '../components/UserCard.vue';
 
 const shortUserList = computed(() => {
   return users.value.slice(0, 5)
+
+})
+
+const usersStore = UsersStore();
+usersStore.fetchUsers();
+
+const newShortUserList = computed(() => {
+  return usersStore.newUsers.slice(0, 5)
 
 })
 
@@ -17,10 +27,10 @@ const shortUserList = computed(() => {
     This is a place to manage various things: todos, users, posts, etc.<br>
     Whatever your minde desires!
   </p>
-  <button @click="changeFirstUser">Change First User</button>
-  <button @click="changeSecondUser">Change Second User</button>
+  <button @click="usersStore.newChangeFirstUser">Change First User</button>
+  <button @click="usersStore.newChangeSecondUser">Change Second User</button>
   <UserCard
-  v-for="user in shortUserList"
+  v-for="user in newShortUserList"
   :key="`user-${user.id}`"
   :user="user"
   class="user-card"
