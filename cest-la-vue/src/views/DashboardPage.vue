f82237b1cf7c0e5705b5ad69ff419e2fd8904fdd
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 11:55:53 +0200

    Dynamic Routing Exercice: create route and view

---
+++ cest-la-vue/src/views/DashboardPage.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { users } from '@/composables/useUserStore';
+  import { useRoute } from "vue-router";
+  const route = useRoute();
+  const id = route.params.id;
+
+  defineProps({
+    title: {
+      type: String,
+      default: 'Users'
+    }
+  })
+
+  console.log(users);
+
+  </script>
+
+  <template>
+    <h1>Dashboard</h1>
+    <!-- <UserCard
+      v-for="user in users"
+      :key="`user-${user.id}`"
+      :user="user"
+      class="user-card"
+    /> -->
+  </template>
+
+  <style>
+
+  </style>
--- sandbox/src/views/PokemonPage.vue
+++ sandbox/src/views/PokemonPage.vue
@@ -1,7 +1,7 @@
 <script setup>
-    import { useRoute } from "vue-router";
-    const route = useRoute();
-    const id = route.params.id;
+  import { useRoute } from "vue-router";
+  const route = useRoute();
+  const id = route.params.id;
 </script>
 <template>
   <h1>Pokemon {{ id }} </h1>
