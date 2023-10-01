3de2629839fef50aeda4570c422f62dd1b3bc291
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 17:41:15 +0200

    Forgot to save Dashboard page

--- cest-la-vue/src/views/DashboardPage.vue
+++ cest-la-vue/src/views/DashboardPage.vue
@@ -1,27 +1,27 @@
 <script setup>
-  import UserCard from '@/components/UserCard.vue';
-  // import { users } from '@/composables/useUserStore';
-  import { useUserStore } from "@/stores/UserStore";
-  import { useRoute } from "vue-router";
-  const route = useRoute();
-  // const id = parseInt(route.params.id, 10);
-  const name = route.params.name
+import UserCard from '@/components/UserCard.vue';
+// import { users } from '@/composables/useUserStore';
+import { useUserStore } from "@/stores/UserStore";
+import { useRoute } from "vue-router";
+const route = useRoute();
+// const id = parseInt(route.params.id, 10);
+const name = route.params.name
 
-  const userStore = useUserStore();
+const userStore = useUserStore();
 
-  const userFound = userStore.userList.find(user => user.name === name);
-  console.log(userFound);
+const userFound = userStore.userList.find(user => user.name === name);
+console.log(userFound);
 
-  </script>
+</script>
 
-  <template>
-    <h1>Dashboard</h1>
-    <h3>{{ userFound.id }}. {{ userFound.name }}</h3>
-    <p>Username: {{ userFound.username }}</p>
-    <p>Email: {{ userFound.email }}</p>
-    <p>Phone: {{ userFound.phone }}</p>
-  </template>
+<template>
+  <h1>Dashboard</h1>
+  <h3>{{ userFound.id }}. {{ userFound.name }}</h3>
+  <p>Username: {{ userFound.username }}</p>
+  <p>Email: {{ userFound.email }}</p>
+  <p>Phone: {{ userFound.phone }}</p>
+</template>
 
-  <style>
+<style>
 
-  </style>
+</style>
