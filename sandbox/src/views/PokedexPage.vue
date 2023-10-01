573f03800f03b54db471bb6655c3c4444ab022ea
gwendal le bris <gwen.coding@gmail.com>
2023-10-01 11:48:18 +0200

    Try a test - will come back to it later

--- sandbox/src/views/PokedexPage.vue
+++ sandbox/src/views/PokedexPage.vue
@@ -1,7 +1,16 @@
 <script setup>
+  // TEST PAS REUSSI
+  // import { ref } from 'vue';
+  // const pokemonId = ref(1);
+  // console.log(pokemonId.value);
 </script>
 <template>
   <h1>Pokedex Page</h1>
-  <router-link to="/pokedex/20">Go to Pokemon</router-link>
+  <div>
+    <!-- TEST PAS REUSSI -->
+    <!-- <input type="number" value={{ pokemonId.value }}> -->
+  </div>
+  <!-- <router-link :to="'/pokedex/' + pokemonId.value">Go to Pokemon {{ pokemonId }} </router-link> -->
+  <router-link to='/pokedex/20'>Go to Pokemon </router-link>
 </template>
 <style></style>
